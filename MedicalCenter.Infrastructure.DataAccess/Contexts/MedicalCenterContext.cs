6b7323414df597d324ce1cbcdcbd81fa675b5b98
GomeesRicardo <52104814+GomeesRicardo@users.noreply.github.com>
2019-06-25 13:25:33 -0300

    Add files via upload

--- MedicalCenter.Infrastructure.DataAccess/Contexts/MedicalCenterContext.cs
+++ MedicalCenter.Infrastructure.DataAccess/Contexts/MedicalCenterContext.cs
@@ -2,6 +2,8 @@
 using MedicalCenter.DomainModel.ValueObjects;
 //using MedicalCenter.Infrastructure.DataAccess.Contexts.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,10 +12,10 @@ namespace MedicalCenter.Infrastructure.DataAccess.Contexts
 {
     public class MedicalCenterContext : DbContext
     {
-        public DbSet<Clinica> Clinicas { get; set; }
-        public DbSet<Paciente> Pacientes { get; set; }
-        public DbSet<Exame> Exames { get; set; }
-        public DbSet<Agendamento> Agendamentos { get; set; }
+        public DbSet<Clinicas> Clinicas { get; set; }
+        public DbSet<Pacientes> Pacientes { get; set; }
+        public DbSet<Exames> Exames { get; set; }
+        public DbSet<Agendamentos> Agendamentos { get; set; }
 
         public MedicalCenterContext()
         {
@@ -30,23 +32,42 @@ namespace MedicalCenter.Infrastructure.DataAccess.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //modelBuilder.RemovePluralizingTableNameConvention();
+
             base.OnModelCreating(modelBuilder);
 
             modelBuilder
-                .Entity<Paciente>()
-                .Property(paciente => paciente.Endereco)
+                .Entity<Pacientes>()
+                .Property(paciente => paciente.endereco)
                 .HasConversion(
                     endereco => endereco.ToString(),
                     endereco => Endereco.Parse(endereco))
                 .HasColumnName("endereco");
 
             modelBuilder
-                .Entity<Clinica>()
-                .Property(clinica => clinica.Endereco)
+                .Entity<Clinicas>()
+                .Property(clinica => clinica.endereco)
                 .HasConversion(
                     endereco => endereco.ToString(),
                     endereco => Endereco.Parse(endereco))
                 .HasColumnName("endereco");
+
+
+            //modelBuilder.Entity<Agendamentos>()
+            //   .HasOne(p => p.Exames)
+            //   .WithMany(b => b.Agendamentos)
+            //   .HasForeignKey(p => p.IdExame);
+        }
+    }
+}
+
+public static class ModelBuilderExtensions
+{
+    public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+    {
+        foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+        {
+            entity.Relational().TableName = entity.DisplayName();
         }
     }
 }
